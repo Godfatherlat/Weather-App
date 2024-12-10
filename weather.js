e5fd3435cf4ca99f924d5117adcbb9b619814e19
Varun Ghatage <varunghatage1602@gmail.com>
2024-12-10 23:27:43 +0530

    check api keys

--- weather.js
+++ weather.js
@@ -1,17 +1,16 @@
 const apiKey="eaa8c72be6bfde3d4315b04842469439";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?&";
+
+//https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=eaa8c72be6bfde3d4315b04842469439
+
+
 
 const searchBox=document.querySelector(".search input");
-
 const searchBtn=document.querySelector(".search btn");
-
-
-
-
 
 async function checkWeather() {
     const response=await fetch(apiUrl+`&appid=${apiKey}`);
-    var data=await response.json();
+    var data= await response.json();
     console.log(data);
 
 
@@ -20,17 +19,11 @@ document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
 document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
 document.querySelector(".wind").innerHTML=data.main.speed+"km/hr";
 
-
-// console.log(document.querySelector(".city").innerHTML=data.name);
-// console.log(document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C");
-// console.log(document.querySelector(".humidity").innerHTML=data.main.humidity+"%");
-// console.log(document.querySelector(".wind").innerHTML=data.main.speed+"km/hr");
-
-
-
-
     
 }
+
+console.log(searchBox.value);
+
 
 checkWeather();
 
