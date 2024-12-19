5a3773a42670bc35b5b768e0e6f554fc984809f9
Varun Ghatage <varunghatage1602@gmail.com>
2024-12-19 15:46:56 +0530

    All details are displayed except wind speed

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const apiKey="eaa8c72be6bfde3d4315b04842469439";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metricq=";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 //https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=eaa8c72be6bfde3d4315b04842469439
 
@@ -9,17 +9,26 @@ const searchBox=document.querySelector(".search input");
 const searchBtn=document.querySelector(".search button");
 
 async function checkWeather( city) {
-    // const response=await fetch(apiUrl+`&appid=${apiKey}`);
+ // const response=await fetch(apiUrl+`&appid=${apiKey}`);
     const response=await fetch(apiUrl+city+`&appid=${apiKey}`);
     
     var data= await response.json();
-    console.log(data);
+    console.log(data.name);
+    
+    console.log(data.main.temp+"°C");
+    console.log(data.main.humidity+"%");
+    console.log(data.wind.speed+"km/hr");
+    let input_city=document.getElementById("in_city");
+    console.log(input_city.value);
+    
+    
 
-
-    document.querySelector("#city").innerHTML=data.name;
+    document.querySelector(".city__name").innerHTML=data.name;
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
     document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-    document.querySelector(".wind").innerHTML=data.main.speed+"km/hr";
+    console.log(data);
+    
+        document.querySelector(".wind_speed").innerHTML=data.wind.speed+"km/hr";
 }
 
 
@@ -32,8 +41,7 @@ searchBtn.addEventListener("click",()=>{
     });
 
 
-    // let input_city=document.getElementById("in_city");
-    // console.log(input_city.value);
+    
     
 
 
